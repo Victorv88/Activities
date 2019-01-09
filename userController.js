139b0f3f21211163946fe5cd9e5a7e9682bd71d4
MarianC10 <marian_cons@yahoo.com>
2019-01-09 20:34:11 +0200

    -added registration validation

--- userController.js
+++ userController.js
@@ -1,5 +1,6 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var validator = require('validator');
 const uuidv4 = require('uuid/v4');
 
 var parser = bodyParser.urlencoded({extended: false});
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://eventsapp:eventsapp123@ds235833.mlab.com:35833/event
 var dataSchema = new mongoose.Schema({
   username: String,
   password: String,
+  email: String,
   token: String,
   ownedActivities: Array,
   joinedActivities: Array
@@ -17,48 +19,65 @@ var dataSchema = new mongoose.Schema({
 var Data = mongoose.model("UsersData", dataSchema);
 
 module.exports.main = function(app) {
-  var sess;
   app.get('/register', parser, function(req, res){
-    sess = req.session;
-    if (sess.token) {
+    if (req.session.token) {
       res.redirect('/');
     }
     else {
-      res.render('register');
+      res.render('register', {errors: null});
     }
   });
   app.post('/register', parser, function(req, res) {
-    /// Search for user's data to check if they are already registered or the username is taken
-    Data.findOne({username: req.body.username}).then(function(result) {
-      /// If they are not already registered, save his account data, otherwise tell the user the name is already taken/the account is already registered
-      if (result === null) {
-        /// Generate a unique token used for the sesssion
-        var newToken = uuidv4();
-        var message = 'You have registered successfully!';
-        var title = 'Succes';
-        console.log(newToken);
-        var newUserData = Data({username: req.body.username, password: req.body.password, token: newToken}).save(function(err, data) {
-          if (err) throw err;
-        });
-        res.render('messagePage', {message: message, title: title});
-      }
-      else {
-        if (result.password === req.body.password) {
-          var message = 'You are already registered!';
-          var title = 'Already registered';
+    var errors = [];
+    if (req.body.password !== req.body.confirmPassword) {
+      errors.push('The passwords do not match.');
+    }
+    if (!validator.isLength(req.body.password, {min: 6})) {
+      errors.push('The password must be at least 6 characters long.');
+    }
+    if (!validator.isEmail(req.body.email)) {
+      errors.push('The email is invalid.');
+    }
+    if (errors.length > 0) {
+      req.session.errors = errors;
+      res.render('register', {errors: errors});
+    }
+    else {
+      /// Search for user's data to check if they are already registered or the username is taken
+      Data.findOne({$or: [{username: req.body.username}, {email: req.body.email}]}).then(function(result) {
+        /// If they are not already registered, save his account data, otherwise tell the user the name is already taken/the account is already registered
+        if (result === null) {
+          /// Generate a unique token used for the sesssion
+          var newToken = uuidv4();
+          var message = 'You have registered successfully!';
+          var title = 'Succes';
+          console.log(newToken);
+          var newUserData = Data({username: req.body.username, password: req.body.password, email: req.body.email, token: newToken}).save(function(err, data) {
+            if (err) throw err;
+          });
           res.render('messagePage', {message: message, title: title});
         }
         else {
-          var message = 'The username is already taken!';
-          var title = 'Username taken';
-          res.render('messagePage', {message: message, title: title});
+          if (result.password === req.body.password && result.email === req.body.email && result.username === req.body.username) {
+            errors.push('You are already registered!');
+            res.render('register', {errors: errors});
+          }
+          else {
+            if (result.email === req.body.email) {
+              errors.push('The email is already in use.');
+            }
+            if (result.username === req.body.username) {
+              errors.push('The username is already taken.');
+            }
+            res.render('register', {errors: errors});
+          }
         }
-      }
-    });
+      });
+    }
   });
   app.get('/login', parser, function(req, res) {
     sess = req.session;
-    if (sess.token) {
+    if (req.session.token) {
       res.redirect('/');
     }
     else {
@@ -77,8 +96,8 @@ module.exports.main = function(app) {
         res.render('messagePage', {message: message, title: title});
       }
       else {
-        sess.token = result.token;
-        sess.username = result.username;
+        req.session.token = result.token;
+        req.session.username = result.username;
         console.log('Found.');
         res.redirect('/');
       }
@@ -97,42 +116,14 @@ module.exports.main = function(app) {
 };
 
 module.exports.updateOwnedEvents = function(req, eventToken) {
-  var sess = req.session;
-  /**Data.findOne({token: sess.token}).then(function(result) {
-    var newActivities = [];
-    newActivities = result.ownedActivities;
-    newActivities.push(eventToken);
-    Data.updateOne({token: sess.token}, {$set: {ownedActivities: newActivities}})
-    .exec()
-    .then(function(result) {
-      console.log("List of owned events updated.");
-    })
-    .catch(function(err) {
-      throw(err);
-    });
-  });*/
-  Data.findOneAndUpdate({token: sess.token}, {$push: {ownedActivities: eventToken}}, function(err, result){
+  Data.findOneAndUpdate({token: req.session.token}, {$push: {ownedActivities: eventToken}}, function(err, result){
     if (err) throw err;
     console.log('List of joined events updated.');
   });
 };
 
 module.exports.updateJoinedEvents = function(req, eventToken) {
-  var sess = req.session;
-  /**Data.findOne({token: sess.token}).then(function(result) {
-    var newActivites = [];
-    newActivities = result.joinedActivities;
-    newActivities.push(eventToken);
-    Data.updateOne({token: sess.token}, {$set: {joinedActivities: newActivities}})
-    .exec()
-    .then(function(result) {
-      console.log('List of joined events updated.');
-    })
-    .catch(function(error) {
-      throw(err);
-    });
-  });*/
-  Data.findOneAndUpdate({token: sess.token}, {$push: {joinedActivities: eventToken}}, function(err, result){
+  Data.findOneAndUpdate({token: req.session.token}, {$push: {joinedActivities: eventToken}}, function(err, result){
     if (err) throw err;
     console.log('List of joined events updated.');
   });
